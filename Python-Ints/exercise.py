607f29a09350c2d92f47801e737de39364747bd1
Praful-a <prafuldubey27@gmail.com>
2020-03-26 21:56:12 +0530

    add the exercise file in python-Ints folder

---
+++ Python-Ints/exercise.py
@@ -0,0 +1,66 @@
+# num = 3
+
+# type funciton use to see the type of your variable.
+print(type(num))
+
+# Addition
+print(3 + 2)
+
+# Substraction
+print(3 - 2)
+
+# Multiplication
+print(3 * 2)
+
+# Division
+print(3 / 2)
+
+# Floor Division
+print(3 // 2)
+
+# Powers
+print(3 ** 2)
+
+# Modulus
+print(3 % 2)
+print(3 % 2)
+print(4 % 2)
+print(5 % 2)
+
+# Orders in python
+# In programming order terms this will use our BODMAS Rules.
+print(3 * (2 + 1))
+
+num = 1
+# num = num + 1
+num += 1
+print(num)
+
+num *= 10
+print(num)
+# This abs funciton in python tell us the absolute values.
+print(abs(-3))
+# return 3 in the output
+
+print(round(3.75, 1))
+This is help funciton use to see any kind of help according to the function in python.
+help(round)
+
+# Comparisons
+num_1 = 3
+num_2 = 2
+
+print(num_1 == num_2)
+print(num_1 > num_2)
+print(num_1 < num_2)
+print(num_1 <= num_2)
+print(num_1 >= num_2)
+
+num_1 = '100'
+num_2 = '200'
+
+# Here we are using casting.
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+print(num_1 + num_2)
