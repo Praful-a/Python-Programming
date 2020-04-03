8e7188cc61fdf332fab7cb56cd605ddd786d24cf
Praful-a <prafuldubey27@gmail.com>
2020-04-03 16:18:16 +0530

    Added slicing in python.

---
+++ slicing#List#strings/slicing.py
@@ -0,0 +1,31 @@
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# +  0  1  2  3  4  5  6  7  8  9
+# - -10 -9 -8 -7 -6 -5 -4 -3 -2 -1
+
+list[start:end:step]
+
+print(my_list[5])
+print(my_list)
+print(my_list[0:5])
+print(my_list[0:6])
+print(my_list[3:8])
+print(my_list[-7:-2])
+print(my_list[1:-2])
+print(my_list[1:-9])
+print(my_list[1:])
+print(my_list[5:])
+print(my_list[:-1])
+print(my_list[:])
+
+print(my_list[2:-1:2])
+
+print(my_list[2:-1:1])
+print(my_list[-1:2:-1])
+print(my_list[-2:1:-1])
+print(my_list[::-1])
+
+sample_url = 'http://coreyms.com'
+print(sample_url)
+print(sample_url[-4:])
+print(sample_url[7:])
+print(sample_url[7:-4])
