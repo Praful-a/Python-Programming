abe18831badbac417e3f1bf5a1bda54c4a0d620d
praful-cyber <mca20017@glbitm.ac.in>
2021-04-10 22:18:57 +0530

    solved count freq string que.

--- Programs/strings/problems.py
+++ Programs/strings/problems.py
@@ -6,6 +6,7 @@
 # print(count)
 
 # count the frequency of each character in string
+## Naive method
 # def count_freq(str):
 #     dict = {}
 #     for n in str:
@@ -17,13 +18,20 @@
 #     return dict
 # print(count_freq("geeksforgeeks"))
 
-def count_freq(str):
-    count = 0
-    for i in range(len(str)):
-        for j in range(i+1, len(str)):
-            if str[i] != str[j]:
-                count = 1
-            else:
-                count += 1
-    print("{} : {}".format(str[i], count))
-count_freq("geeksforgeeks")
+## using collections.Counter()
+# from collections import Counter
+# test_str = "geeksforgeeks"
+# res = Counter(test_str)
+# print("Count of all characters in GeeksforGeeks is :\n " + str(res))
+
+# using dictionary get method
+# test_str = "GeeksforGeeks"
+# res = {}
+# for keys in test_str:
+#     res[keys] = res.get(keys, 0) + 1
+# print("Count of all characters in GeeksforGeeks is : \n" + str(res))
+
+## using set + count method
+test_str = "GeeksforGeeks"
+res = {i : test_str.count(i) for i in set(test_str)}
+print("The count of all characters in GeeksforGeeks is :\n" + str(res))
