a983292a057d8efbf5e1e98d37a7626ff1dce486
praful-cyber <mca20017@glbitm.ac.in>
2021-04-11 12:27:01 +0530

    solved multiply all list items problem.

--- Programs/List/problem.py
+++ Programs/List/problem.py
@@ -6,10 +6,26 @@
 #         sum += li[i]
 #     return sum
 # print(sum_all_item([1,2,3,4,5]))
+### Second Method ####
+# def sum_all_item(li):
+#     sum = 0
+#     for i in li:
+#         sum += i
+#     return sum
+# print(sum_all_item([1,2,3,4,5]))
 
-def sum_all_item(li):
-    sum = 0
-    for i in li:
-        sum += i
-    return sum
-print(sum_all_item([1,2,3,4,5]))
+# multiplies all the items in a list
+"""First Method"""
+# def mul_all_items(l):
+#     product = 1
+#     for i in range(len(l)):
+#         product *= l[i]
+#     return product
+# print(mul_all_items([1,2,3,4,5]))
+"""Second Method"""
+def mul_all_items(l):
+    product = 1
+    for i in l:
+        product *= i
+    return product
+print(mul_all_items([1,2,3]))
