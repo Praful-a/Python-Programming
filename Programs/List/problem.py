dd2986d60a66d092dde877d0d6cd54b52890c3e0
praful-cyber <mca20017@glbitm.ac.in>
2021-04-11 15:11:59 +0530

    sort a tuple by their last element.

--- Programs/List/problem.py
+++ Programs/List/problem.py
@@ -88,3 +88,33 @@ list of strings."""
 #                 count += 1
 #     return count
 # print(count_string(['aba', 'xyz', 'aba', '1221']))
+
+"""Sort in increasing order by the last element in each tuple
+from a given list of non-empty tuples."""
+"""using sorted Method"""
+# def last(n):
+#     return n[-1]
+# def sort_list_last(tuples):
+#     return sorted(tuples, key=last)
+# print(sort_list_last( [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]))
+"""Using Bubble sort"""
+# def sort_Tuple(tup):
+#     lst = len(tup)
+#     for i in range(0, lst):
+#         for j in range(0, lst-i-1):
+#             if (tup[j][-1] > tup[j+1][-1]):
+#                 temp = tup[j]
+#                 tup[j] = tup[j+1]
+#                 tup[j+1] = temp
+#     return tup
+# tup =  [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+# print(sort_Tuple(tup))
+"""Using sort method"""
+# def sort_Tuple(tup):
+#     tup.sort(key=lambda x: x[-1])
+#     return tup
+# tup =  [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+# print(sort_Tuple(tup))
+
+"""Remove Duplicates from  a list."""
+    
