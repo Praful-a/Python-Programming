22579b6932fc2fa199bbb6c3a3c50d9055a45649
praful-cyber <mca20017@glbitm.ac.in>
2021-04-11 12:20:32 +0530

    solved sum all list items problem.

--- Programs/List/problem.py
+++ Programs/List/problem.py
@@ -1,7 +1,15 @@
 # Program to sum all the items in a list
+## 1st Method  ##
+# def sum_all_item(li):
+#     sum = 0
+#     for i in range(len(li)):
+#         sum += li[i]
+#     return sum
+# print(sum_all_item([1,2,3,4,5]))
+
 def sum_all_item(li):
     sum = 0
-    for i in range(len(li)):
-        sum += li[i]
+    for i in li:
+        sum += i
     return sum
 print(sum_all_item([1,2,3,4,5]))
